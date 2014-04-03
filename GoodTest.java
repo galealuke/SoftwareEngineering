41b98293ecba7b1212ddc223c6914b9236c135d6
Luke <galea.luke@gmail.com>
2014-04-03 12:51:49 +0200

    added calculator and test

---
+++ GoodTest.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import calculator.Calculator;
+
+public class GoodTest {
+
+	@Test
+public void additionTest(){
+		Calculator calculator = new Calculator();
+		assertEquals(4, calculator.add(2, 2));
+		assertEquals(5, calculator.subtract(7, 2));
+	}
+
+}
