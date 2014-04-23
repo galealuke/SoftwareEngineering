51e91e217333c2c7c9483472ccc3059f03e41781
Luke <galea.luke@gmail.com>
2014-04-23 21:23:49 +0200

    added game test template

---
+++ src/test/java/com/pest/demo/GameTest.java
@@ -0,0 +1,22 @@
+package com.pest.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestGame {
+
+	Game game;
+	
+	@Before
+	public void setUp() throws Exception {
+		game= new Game();
+	}
+
+	@Test
+	public void test() {
+		//assertEquals(game.gameStart);
+	}
+	
+}
